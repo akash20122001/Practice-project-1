4719623f9a9777c2418ea1c6f7b6d2a6f6bff55a
Akash <akash8882992242@gmail.com>
2024-01-29 14:42:36 +0530

    Add States for Posts

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -6,35 +6,35 @@ import Sidebar from "./Components/Sidebar";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import CreateForm from "./Components/CreateForm";
-import Card from "./Components/Card";
 import Posts from "./Components/Posts";
 
 function App() {
+  const [flag, setFlag] = useState("show");
 
-  const [flag, setFlag] = useState("show")
-  
-  function handleOnClick(chosenButton)  {
-    if(chosenButton === "show") setFlag("show")
-    else if(chosenButton === "create") setFlag("create")
+  const [posts, setPosts] = useState([
+    { name: "Akash", description: "Hello how are You?" },
+    {
+      name: "Mayank",
+      description: "What a lovely day :)",
+    },
+  ]);
+
+  function handleOnClick(chosenButton) {
+    if (chosenButton === "show") setFlag("show");
+    else if (chosenButton === "create") setFlag("create");
     console.log(flag);
   }
 
   return (
     <>
       <div className="flex h-screen w-full bg-blue-300">
-        {/* sidebar */}
-        <Sidebar onClickButton = {handleOnClick} />
+        <Sidebar onClickButton={handleOnClick} />
         <div className="flex flex-col justify-between w-[80%]">
-          {/* header */}
+          <Header />
 
-          <Header/>
+          {flag === "show" ? <Posts posts = {posts} /> : <CreateForm />}
 
-          {
-            flag==="show" ? <Posts/> : <CreateForm/>
-          }
-
-          {/* footer */}
-          <Footer/>
+          <Footer />
         </div>
       </div>
     </>
--- my-project/src/Components/Posts.jsx
+++ my-project/src/Components/Posts.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Card from "./Card";
 
-function Posts() {
+function Posts({ posts }) {
   return (
     <div className="pl-6 py-4">
-      <Card />
-      <Card />
-      <Card />
+      {posts.map((post, index) => (
+        <Card key={index} 
+            name={post.name} 
+            description={post.description} />
+      ))}
     </div>
   );
 }
